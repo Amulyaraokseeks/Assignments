6fdeeddff477515a8800d7c3972a640d17579c7e
Amulyaraokseeks <amy.crony@gmail.com>
2023-11-03 11:48:36 -0400

    Assignment submission

---
+++ movingfiles.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#display the explanation of command
+echo "ls This command List files and directories"
+
+echo "ls -l displays files and directories with detailed information"
+cd
+echo "Change directory"
+ pwd 
+echo "Print the current working directory"
+
+echo "mkdir -p Create a new directory"
+
+echo "cp Copy files and directories"
+
+echo "mv Move/rename files and directories"
+
+echo "touch Create an empty file or update file timestamps"
+
+echo "cat View the contents of a file"
+
+
+
